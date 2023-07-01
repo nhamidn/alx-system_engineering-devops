fa23eda3061129dc8f0075bbf6c3ae8d071aa595
nhamidn <hamid-savana@hotmail.fr>
2023-07-01 03:35:31 +0100

    templates

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the init function of the program
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	return (0);
+}
