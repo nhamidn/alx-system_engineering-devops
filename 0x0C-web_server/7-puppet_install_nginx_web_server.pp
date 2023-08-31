67d2ea5b8ce555a566f658ba928672b5f6e2a9c7
nhamidn <hamid-savana@hotmail.fr>
2023-08-31 03:14:12 +0100

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,6 +8,13 @@ file { '/var/www/html/index.html':
   content => 'Hello World',
 }
 
+file_line { 'redirecting':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
