263518ccfdbf5c2c072735d471c7341a8eaa4892
nhamidn <hamid-savana@hotmail.fr>
2023-08-31 03:04:05 +0100

    revert change

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,28 +8,6 @@ file { '/var/www/html/index.html':
   content => 'Hello World',
 }
 
-file_line { 'nginx-redirect':
-  path   => '/etc/nginx/sites-available/default',
-  line   => '    location /redirect_me {',
-  match  => '^server {',
-  after  => '^server {',
-  ensure => 'present',
-}
-
-file_line { 'nginx-redirect-return':
-  path   => '/etc/nginx/sites-available/default',
-  line   => '        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;',
-  after  => '    location /redirect_me {',
-  ensure => 'present',
-}
-
-file_line { 'nginx-redirect-end':
-  path   => '/etc/nginx/sites-available/default',
-  line   => '    }',
-  after  => '        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;',
-  ensure => 'present',
-}
-
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
